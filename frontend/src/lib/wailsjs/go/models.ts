0142529ea793df0d0d7a22ddde16f11944e83830
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2025-09-23 15:46:59 +0200

    feat: autogenerate

--- frontend/src/lib/wailsjs/go/models.ts
+++ frontend/src/lib/wailsjs/go/models.ts
@@ -25,6 +25,7 @@ export namespace frontend {
 	    updatedAt: any;
 	    name: string;
 	    projectId: number;
+	    environmentId: number;
 	
 	    static createFrom(source: any = {}) {
 	        return new CollectionDto(source);
@@ -36,6 +37,7 @@ export namespace frontend {
 	        this.updatedAt = this.convertValues(source["updatedAt"], null);
 	        this.name = source["name"];
 	        this.projectId = source["projectId"];
+	        this.environmentId = source["environmentId"];
 	    }
 	
 		convertValues(a: any, classs: any, asMap: boolean = false): any {
@@ -56,6 +58,81 @@ export namespace frontend {
 		    return a;
 		}
 	}
+	export class EnvironmentHeaderDTO {
+	    id: number;
+	    // Go type: time
+	    updatedAt: any;
+	    key: string;
+	    value: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new EnvironmentHeaderDTO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.updatedAt = this.convertValues(source["updatedAt"], null);
+	        this.key = source["key"];
+	        this.value = source["value"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class EnvironmentDTO {
+	    id: number;
+	    // Go type: time
+	    updatedAt: any;
+	    name: string;
+	    header: EnvironmentHeaderDTO[];
+	
+	    static createFrom(source: any = {}) {
+	        return new EnvironmentDTO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.updatedAt = this.convertValues(source["updatedAt"], null);
+	        this.name = source["name"];
+	        this.header = this.convertValues(source["header"], EnvironmentHeaderDTO);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	
 	export class HttpRequestBodyDto {
 	    id: number;
 	    // Go type: time
@@ -185,6 +262,7 @@ export namespace frontend {
 	    body: HttpRequestBodyDto;
 	    parameter: HttpRequestParameterDto[];
 	    header: HttpRequestHeaderDto[];
+	    disabledEnvironmentHeader: number[];
 	
 	    static createFrom(source: any = {}) {
 	        return new HttpRequestDto(source);
@@ -202,6 +280,7 @@ export namespace frontend {
 	        this.body = this.convertValues(source["body"], HttpRequestBodyDto);
 	        this.parameter = this.convertValues(source["parameter"], HttpRequestParameterDto);
 	        this.header = this.convertValues(source["header"], HttpRequestHeaderDto);
+	        this.disabledEnvironmentHeader = source["disabledEnvironmentHeader"];
 	    }
 	
 		convertValues(a: any, classs: any, asMap: boolean = false): any {
